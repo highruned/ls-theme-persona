896e20d8864289402ef6b0d930c0aec462905f0e
Eric Muyser <eric@muyser.com>
2011-09-25 00:57:38 -0700

    clean up.

--- templates/pages/sitemap/pre_action.php
+++
@@ -1,3 +0,0 @@
-<?
-header("Content-Type: application/xml");
-?>
--- templates/templates/general.php
+++ templates/templates/general.php
@@ -21,5 +21,4 @@
       </ul>
     </div>
     <div id="loading" class="top"><span>Working...</span></div>
-    <a href="https://github.com/ericmuyser/ls-theme-persona"><img style="position: absolute; top: 0; left: 0; z-index: 99999; border: 0;" src="https://assets1.github.com/img/c641758e06304bc53ae7f633269018169e7e5851?repo=&url=http%3A%2F%2Fs3.amazonaws.com%2Fgithub%2Fribbons%2Fforkme_left_white_ffffff.png&path=" alt="Fork me on GitHub"></a>
     <? $this->render_partial('site:foot') ?>
